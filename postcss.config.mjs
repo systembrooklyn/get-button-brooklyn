b4bf3e83dd9ab0c80ffd61a67a1671b997e62cef
emad-desoky <deskoemad@gmail.com>
2025-11-30 17:43:56 +0200

    1

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,19 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Increase limit to handle Base64 file uploads
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default config;
